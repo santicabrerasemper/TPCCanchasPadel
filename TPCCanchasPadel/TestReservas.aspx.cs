4546480439ee87551521d846d8cd5109fb0355ba
santicabrerasemper <santicabrerasemper8@gmail.com>
2025-10-29 11:40:13 -0300

    se sacan elementos

---
+++ TPCCanchasPadel/TestReservas.aspx.cs
@@ -0,0 +1,23 @@
+﻿using ConexionBD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TPCCanchasPadel
+{
+    public partial class TestReservas : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                var reservasBD = new ReservasCanchas();
+                gvReservas.DataSource = reservasBD.ObtenerTodasLasReservas();
+                gvReservas.DataBind();
+            }
+        }
+    }
+}
